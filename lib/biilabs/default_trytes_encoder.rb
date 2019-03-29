a687fb185ff113d9d3aa0d81c14a8d7bace466c0
gsx <gsx@kdanmobile.com>
2019-03-29 17:47:41 +0800

    add some biilabs-trytes info on README

--- lib/biilabs/default_trytes_encoder.rb
+++ lib/biilabs/default_trytes_encoder.rb
@@ -1,8 +1,24 @@
-# You can write other function to map string to TRYTE_VALUES
+# This class is used to map ASCII string to Trytes format
 #
-# !!!!!!! Waring !!!!!!
-# for this map-function
-# all input chars must be a "ASCII character"
+# Trytes format means :
+#      all cart must be on of '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#
+# Maybe You can find your way to do this.
+
+
+# !!!!!!!!!!!!!
+# for this map-function :
+#   1. all input chars must be a "ASCII character"
+#   2. length of string will *2 after encode to trytes.
+# * 3. this function include some rule for working on Biilbas
+#      1). after you post trytes string to Biilabs
+#          if fill your string to max-length with '9'.
+#          ASCII : null   <=>   Trytes : 99
+#          example :
+#            max-length-of-trytes : 27
+#            string          : 'a1'
+#            trytes-encode   : 'PCVA' (alaways even-length)
+#            data-on-biilabs : 'PCVA99999999999999999999999' (odd-length)
 
 module Biilabs
   class DefaultTrytesEncoder
